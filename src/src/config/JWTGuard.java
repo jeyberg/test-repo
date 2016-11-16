8e01391c7112d88a229470108a67e09c65439e36
Johann <johann.andrejtschik@novatec-gmbh.de>
2016-11-16 13:37:50 +0100

    implemented JWT with Apache Shiro

---
+++ src/src/config/JWTGuard.java
@@ -0,0 +1,17 @@
+package src.config;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.shiro.web.filter.authc.AuthenticationFilter;
+
+public class JWTGuard extends AuthenticationFilter {
+
+	@Override
+	protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		httpResponse.sendError(HttpServletResponse.SC_FORBIDDEN);
+		return false;
+	}
+}
